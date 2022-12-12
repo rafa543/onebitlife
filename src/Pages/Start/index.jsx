a714cbb246683b1bc3be86bf4b2cfff49790d0a6
Rafael Araujo <rafa123839@gmail.com>
2022-12-12 17:25:15 -0300

    inserindo animacao e estruturando componentes

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
+import LifeStatus from '../../Components/Common/LifeStatus'
 
 
 export default function Start() {
@@ -11,6 +12,7 @@ export default function Start() {
                         source={require("../../assets/icons/logo3.png")} 
                         style={styles.logo}
                     />
+                    <LifeStatus/>
                     <Text style={styles.description}>
                         Vamos transforma sua vida {"\n"} em jogo, buscando sempre {"\n"}
                         o melhor nivel.
